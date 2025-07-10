c2be7f91c9198df86f873ad64ea5439f8d976ed7
Naser Al-Asbahi <nasserqahtan0@gmail.com>
2025-07-10 03:09:42 +0200

    refactoring and stated adding more tools and resources

--- MyFirstMCP/Program.cs
+++ MyFirstMCP/Program.cs
@@ -1,8 +1,7 @@
-﻿using System.ComponentModel;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using ModelContextProtocol.Server;
+
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -14,26 +13,9 @@ builder.Logging.AddConsole(consoleLogOptions =>
 builder.Services
       .AddMcpServer()
       .WithStdioServerTransport()
-      .WithToolsFromAssembly();
+      .WithToolsFromAssembly()
+      .WithResourcesFromAssembly();
 
 await builder.Build().RunAsync();
 
-[McpServerToolType]
-public static class EchoTool
-{
-   [McpServerTool, Description("Echoes the input back to the client.")]
-   public static string Echo(string message)
-   {
-      Console.WriteLine($"Echoing: {message}");
-      return $"hello from c#: {message}";
-   }
 
-   [McpServerTool, Description("Echoes the input back to the client in reverse.")]
-   public static string EchoInReverse(string message)
-   {
-      string reversed = new string(message.Reverse().ToArray());
-      Console.WriteLine($"Echoing in reverse: {reversed}");
-      return reversed;
-   }
-
-}
